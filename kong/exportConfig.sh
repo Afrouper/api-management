84c252746f28ee1bd0e1e2c61c848600d98162b5
Christian Kemper <christian.kemper@atruvia.de>
2024-09-17 11:50:09 +0200

    Add export/import

---
+++ kong/exportConfig.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+KONG_ADMIN_HOST=localhost
+KONG_ADMIN_TOKEN=adminPass
+
+podman run -i --network="host" -v $(pwd):/deck --workdir /deck kong/deck --kong-addr http://${KONG_ADMIN_HOST}:8001 --headers kong-admin-token:${KONG_ADMIN_TOKEN} gateway dump -o /deck/kong_export.yaml --yes
